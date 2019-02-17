03a6b7976a0d515076e09421815b61bbe09916d3
Siyad <mail4siyadmuhammed@gmail.com>
2019-02-17 20:57:10 +0100

    adding page controls

--- lib/story_page.dart
+++ lib/story_page.dart
@@ -3,10 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:nine_stories/models/story.dart';
 
 
-class StoryPage extends StatelessWidget {
+class StoryPage extends StatefulWidget {
   final Story story;
 
   StoryPage({this.story});
+
+  @override
+  _StoryPage createState() => _StoryPage();
+}
+
+class _StoryPage extends State<StoryPage> {
+
+  double _fontFactor = 1;
+  Color _boardColor = Colors.white;
+  Color _textColor = Colors.black;
+  
 
   Widget _buildImageWidget(String image) {
     if (image.isNotEmpty) {
@@ -17,7 +28,7 @@ class StoryPage extends StatelessWidget {
 
   Widget _buildStoryBoard (Story story) {
     return Container(
-      decoration: new BoxDecoration(color: Colors.white),
+      decoration: new BoxDecoration(color: _boardColor),
       child: new SingleChildScrollView(
         child: Column(
           children: <Widget>[
@@ -26,8 +37,8 @@ class StoryPage extends StatelessWidget {
               child: Text(
                 story.title,
                 style: new TextStyle(
-                  fontSize: 22.0,
-                  color: Colors.black,
+                  fontSize: 22.0 * _fontFactor,
+                  color: _textColor,
                 ),
               ),
             ),
@@ -37,8 +48,8 @@ class StoryPage extends StatelessWidget {
               child: Text(
                 story.body,
                 style: new TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.black,
+                  fontSize: 16.0 * _fontFactor,
+                  color: _textColor,
                 ),
               ),
             )
@@ -48,10 +59,92 @@ class StoryPage extends StatelessWidget {
     );
   }
 
+  Widget _buildThemeButtons() {
+    return Row(
+      children: <Widget>[
+        FloatingActionButton(
+          backgroundColor: Colors.black,
+          onPressed: () {
+            setState(() {
+              _boardColor = Colors.black;
+              _textColor = Colors.white;
+            });
+          },
+        ),
+        FloatingActionButton(
+          backgroundColor: Colors.white,
+          onPressed: () {
+            setState(() {
+              _boardColor = Colors.white;
+              _textColor = Colors.black;
+            });
+          },
+        ),
+        FloatingActionButton(
+          backgroundColor: Color(0XFFFFFEEE),
+          onPressed: () {
+            setState(() {
+              _boardColor = Color(0XFFFFFEEE);
+              _textColor = Colors.black;
+            });
+          },
+        ),
+      ],
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    );
+  }
+
+  Widget _buildFontSizeSlider() {
+    return Padding(
+        padding: EdgeInsets.fromLTRB(30, 50, 30, 20),
+        child: Row(
+          children: <Widget>[
+            Text("a", style: new TextStyle(fontSize: 15),),
+            Expanded(
+              child: Slider(
+                activeColor: Color(0xFF167F67),
+                min: 1.0,
+                max: 1.3,
+                onChanged: (newValue) {
+                  setState(() {
+                    _fontFactor = newValue;
+                  });
+                },
+                value: _fontFactor,
+              ),
+            ),
+            Text("a", style: new TextStyle(fontSize: 25),),
+          ],
+        )
+    );
+  }
+
+  Widget _buildBottomModal () {
+    Widget modalBody = Padding(
+      padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          _buildThemeButtons(),
+          _buildFontSizeSlider()
+        ],
+      ),
+    );
+
+    return modalBody;
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget materialPage = Scaffold(
-        body: _buildStoryBoard(story)
+        body: GestureDetector(
+            child: _buildStoryBoard(widget.story),
+            onTap: () {
+              showModalBottomSheet(context: context, builder: (BuildContext context) {
+                return _buildBottomModal();
+              });
+            }
+        )
     );
 
     return materialPage;
